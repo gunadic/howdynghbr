4aadd9b45b0bb12e3d1e11f2f2b38f54a19a780b
Chris Gunadi <gunadic@gmail.com>
2013-06-29 14:27:51 -0400

    adds links to participating users profile page

--- spec/features/user_participation_spec.rb
+++ spec/features/user_participation_spec.rb
@@ -7,7 +7,8 @@ include Warden::Test::Helpers
 # Acceptance Criteria
 # 1) Meetup detail page has an "I'm in!" button.
 # 2) Meetup detail page has list of people who have signed up.
-# 3) User can only sign up for the meetup once.
+# 3) Users can view the profiles of other attendees.
+# 4) User can only sign up for the meetup once.
 
 describe 'signing up for a meetup' do 
 
@@ -36,6 +37,15 @@ describe 'signing up for a meetup' do
     click_on("I'm in!")
     click_on("I'm in!")
     expect(page).to have_content("Hold your horses,")
+  end
+
+  it ", attendees profile view pages are accessible through the details page" do
+    visit meetup_path(@meetup.id)
+    click_on("I'm in!")
+    save_and_open_page
+    click_on(@temp.user_name)
+    expect(page).to have_content(@temp.user_name)
+    expect(page).to have_content(@temp.short_bio)
   end
 
   
