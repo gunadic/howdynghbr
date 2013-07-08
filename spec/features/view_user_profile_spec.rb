a85d8da565cf16662c543c6fff4311a7a8cafc7c
Chris Gunadi <gunadic@gmail.com>
2013-07-08 16:42:52 -0400

    changes application.css to .css.scss and small changes to profile page

---
+++ spec/features/view_user_profile_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+# As a user,
+# I want to have a profile page,
+# so that others can know who I am.
+
+# AC
+# 1) Profile page should include the username, first name/last initial, 
+#   tag line and short-bio
+# 2) Includes a list of past meetups attended
