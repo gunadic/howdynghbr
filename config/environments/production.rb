f91f18fd79b3eab7354ea3ad2af163d488bb8368
Chris Gunadi <gunadic@gmail.com>
2013-07-16 09:02:22 -0400

    adds mailer settings to config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Howdynghbr::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Configure production devise:mailer
+  config.action_mailer.default_url_options = { :host => 'Howdynghbr.heroku.com' }
+
 end
